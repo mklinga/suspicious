dc8e68b947a18678416ce1332e191920ffdeba14
Markus Klinga <klinga.markus@gmail.com>
2020-03-15 14:18:40 +0200

    suspicious/ok

---
+++ ok/precache-manifest.85687dc2b140581f09d54f8cb734cb7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29157de828c6c5ccf17732ba92153f7f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a63cde554732edbd7c56",
+    "url": "/static/css/main.ba73e1e4.chunk.css"
+  },
+  {
+    "revision": "51f3626edc49f748a61b",
+    "url": "/static/js/2.92df2572.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.92df2572.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a63cde554732edbd7c56",
+    "url": "/static/js/main.073a49e9.chunk.js"
+  },
+  {
+    "revision": "5c7155a558e2d075379f",
+    "url": "/static/js/runtime-main.87a3408d.js"
+  }
+]);
